9e95081e112eddab3e6c489a304d4e82bf65fc45
Andy1945 <kuts1945@gmail.com>
2025-09-10 23:26:34 +0300

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/percy',
+  assetPrefix: '/percy/',
+  images: {
+    unoptimized: true,
+  },
   sassOptions: {
     quietDeps: true, // This will silence deprecation warnings
     silenceDeprecations: ["mixed-decls", "legacy-js-api"],
