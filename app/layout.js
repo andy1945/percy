0e1ce972322d43d3a0a72526c43999b91f0f0772
Andy1945 <kuts1945@gmail.com>
2025-09-11 22:28:17 +0300

    responsiveness

--- app/layout.js
+++ app/layout.js
@@ -28,6 +28,7 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
           rel="stylesheet"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
 
       <body>
